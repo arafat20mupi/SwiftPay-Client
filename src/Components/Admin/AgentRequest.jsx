8bae330ffacce5520cdaa9c57329b1545d13b15d
Arafat islam <arafat.innotimes.net@gmail.com>
2024-07-28 18:15:20 +0600

    Admin Page ok : all user , user& admin request , logout

--- src/Components/Admin/AgentRequest.jsx
+++ src/Components/Admin/AgentRequest.jsx
@@ -5,83 +5,91 @@ const AgentRequest = () => {
     const [requests, setRequests] = useState([]);
 
     useEffect(() => {
-      fetchRequests();
+        fetchRequests();
     }, []);
-  
+
     const fetchRequests = async () => {
-      try {
-        const response = await axios.get('http://localhost:5000/requested/agents');
-        setRequests(response.data);
-      } catch (error) {
-        console.error('Error fetching requests:', error);
-      }
+        try {
+            const response = await axios.get('http://localhost:5000/requested/agents');
+            setRequests(response.data);
+        } catch (error) {
+            console.error('Error fetching requests:', error);
+        }
     };
 
     const handleUpdateBalance = async (id) => {
         try {
-          const response = await axios.get(`http://localhost:5000/requested/${id}`);
-          const userDetails = response.data;
-          console.log('User details fetched:', userDetails);
-    
-          const updatedRequest = {
-            balance: 40,
-            status: 'active',
-          };
-    
-          await axios.put(`http://localhost:5000/requested/${id}`, updatedRequest);
-          console.log('User balance updated successfully');
-    
-          const newUserDetails = {
-            ...userDetails,
-            balance: updatedRequest.balance,
-            status: updatedRequest.status,
-          };
-    
-          await axios.post('http://localhost:5000/user', newUserDetails, {
-            withCredentials: true,
-          });
-          alert('User Aprrove successful!');
-          fetchRequests(); // Refresh the list after updating
+            const response = await axios.get(`http://localhost:5000/requested/${id}`);
+            const userDetails = response.data;
+            console.log('User details fetched:', userDetails);
+
+            const updatedRequest = {
+                balance: 10000,
+                status: 'active',
+            };
+
+            await axios.put(`http://localhost:5000/requested/${id}`, updatedRequest);
+            console.log('User balance updated successfully');
+
+            const newUserDetails = {
+                ...userDetails,
+                balance: updatedRequest.balance,
+                status: updatedRequest.status,
+            };
+
+            await axios.post('http://localhost:5000/user', newUserDetails, {
+                withCredentials: true,
+            });
+            alert('User Aprrove successful!');
+            fetchRequests(); // Refresh the list after updating
         } catch (error) {
-          alert('User already approved or error updating balance');
-          console.error('Error updating balance:', error);
+            alert('User already approved or error updating balance');
+            console.error('Error updating balance:', error);
         }
-      };
-    
+    };
+
     return (
-        <div className="container p-2 mx-auto sm:p-4 dark:text-gray-800">
-            <h2 className="mb-4 text-2xl font-semibold leading-tight">User Requests</h2>
-            <div className="overflow-x-auto">
-                <table className="min-w-full text-xs">
-                    <thead className="dark:bg-gray-300">
-                        <tr className="text-left">
-                            <th className="p-3">Name</th>
-                            <th className="p-3">Email</th>
-                            <th className="p-3">Number</th>
-                            <th className="p-3">Role</th>
-                            <th className="p-3">Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {requests.map(request => (
-                            <tr key={request._id} className="border-b border-opacity-20 dark:border-gray-300 dark:bg-gray-50">
-                                <td className="p-3 text-left">{request.name}</td>
-                                <td className="p-3 text-left">{request.email}</td>
-                                <td className="p-3 text-left">{request.number}</td>
-                                <td className="p-3 text-left">{request.role}</td>
-                                <td>
-                                    <button
-                                        className={`btn ${request.status === 'active' ? 'btn-primary ' : 'btn-secondary'}`}
-                                        onClick={() => handleUpdateBalance(request._id)}
-                                    >
-                                        {request.status === 'active' ? 'Approved' : "Requsted "}
-                                    </button>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+        <div>
+            
+            {requests.length === 0 ? (
+                <p className="text-center">No Agent requests found.</p>
+            ) : (
+                <div className="container p-2 mx-auto sm:p-4 ">
+                    <h2 className="mb-4 text-2xl font-semibold text-center leading-tight">Agent Requests</h2>
+                    <div className="overflow-x-auto">
+                        <table className="min-w-full text-xs">
+                            <thead className="">
+                                <tr className="text-left">
+                                    <th className="p-3">Name</th>
+                                    <th className="p-3">Email</th>
+                                    <th className="p-3">Number</th>
+                                    <th className="p-3">Role</th>
+                                    <th className="p-3">Status</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {requests.map((request) => (
+                                    !request.status && (
+                                        <tr key={request._id} className="border-b textlg border-opacity-20 ">
+                                            <td className="p-3 text-left">{request.name}</td>
+                                            <td className="p-3 text-left">{request.email}</td>
+                                            <td className="p-3 text-left">{request.number}</td>
+                                            <td className="p-3 text-left">{request.role}</td>
+                                            <td>
+                                                <button
+                                                    className={`btn btn-primary `}
+                                                    onClick={() => handleUpdateBalance(request._id)}
+                                                >Approve 
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    )
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
--- src/Components/Admin/UserRequest.jsx
+++ src/Components/Admin/UserRequest.jsx
@@ -52,39 +52,47 @@ const UserRequest = () => {
 
 
   return (
-    <div className="container p-2 mx-auto sm:p-4 dark:text-gray-800">
-      <h2 className="mb-4 text-2xl font-semibold leading-tight">User Requests</h2>
-      <div className="overflow-x-auto">
-        <table className="min-w-full text-xs">
-          <thead className="dark:bg-gray-300">
-            <tr className="text-left">
-              <th className="p-3">Name</th>
-              <th className="p-3">Email</th>
-              <th className="p-3">Number</th>
-              <th className="p-3">Role</th>
-              <th className="p-3">Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {requests.map((request) => (
-              <tr key={request._id} className="border-b border-opacity-20 dark:border-gray-300 dark:bg-gray-50">
-                <td className="p-3 text-left">{request.name}</td>
-                <td className="p-3 text-left">{request.email}</td>
-                <td className="p-3 text-left">{request.number}</td>
-                <td className="p-3 text-left">{request.role}</td>
-                <td>
-                  <button
-                    className={`btn ${request.status === 'active' ? 'btn-primary ' : 'btn-secondary'}`}
-                    onClick={() => handleUpdateBalance(request._id)}
-                  >
-                    {request.status === 'active' ? 'Approved' : "Requsted "}
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+    <div>
+      {requests.length === 0 ? (
+        <p className="text-center">No user requests found.</p>
+      ) : (
+        <div className="container  mx-auto sm:p-4 ">
+          <h2 className="mb-4 text-2xl text-center font-semibold leading-tight">User Requests</h2>
+          <div className="overflow-x-auto">
+            <table className="min-w-full text-xs">
+              <thead className="">
+                <tr className="text-left">
+                  <th className="p-3">Name</th>
+                  <th className="p-3">Email</th>
+                  <th className="p-3">Number</th>
+                  <th className="p-3">Role</th>
+                  <th className="p-3">Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                {requests.map((request) => (
+                  !request.status && (
+                    <tr key={request._id} className="border-b border-opacity-20 ">
+                      <td className="p-3 text-left">{request.name}</td>
+                      <td className="p-3 text-left">{request.email}</td>
+                      <td className="p-3 text-left">{request.number}</td>
+                      <td className="p-3 text-left">{request.role}</td>
+                      <td>
+                        <button
+                          className={`btn btn-primary`}
+                          onClick={() => handleUpdateBalance(request._id)}
+                        >
+                          Approve
+                        </button>
+                      </td>
+                    </tr>
+                  )
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
